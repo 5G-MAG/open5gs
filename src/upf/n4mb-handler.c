943b0a680346ef318546425278a01c6becc1ef04
Josep Ribes <jrrm@tutanota.com>
2025-12-15 12:09:37 +0100

    Add multicast route for every PDR

--- src/upf/n4mb-handler.c
+++ src/upf/n4mb-handler.c
@@ -304,20 +304,24 @@ void upf_n4mb_handle_session_establishment_request(
 
 
     if(upf_self()->mbs_multicastrouter_activate){
-        // get the ips from the PDR that gets created
-        uint32_t ip_multicast = created_pdr[0]->ip_multicast_addressing_info.ip_multicast_address.s_ipv4_addr;
-        uint32_t ip_source    = created_pdr[0]->ip_multicast_addressing_info.source_ip_address.ipv4_addr;
+        for (i = 0; i < num_of_created_pdr; i++) {
+            pdr = created_pdr[i];
+            ogs_assert(pdr);
+            // get the ips from the PDR
+            uint32_t ip_multicast = pdr->ip_multicast_addressing_info.ip_multicast_address.s_ipv4_addr;
+            uint32_t ip_source    = pdr->ip_multicast_addressing_info.source_ip_address.ipv4_addr;
 
-        char ip_multicast_string[INET_ADDRSTRLEN];
-        char ip_source_string[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &ip_multicast, ip_multicast_string, sizeof ip_multicast_string);
-        inet_ntop(AF_INET, &ip_source, ip_source_string, sizeof ip_source_string);
+            char ip_multicast_string[INET_ADDRSTRLEN];
+            char ip_source_string[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &ip_multicast, ip_multicast_string, sizeof ip_multicast_string);
+            inet_ntop(AF_INET, &ip_source, ip_source_string, sizeof ip_source_string);
 
-        if(OGS_ERROR == multicastrouter_add_route_and_join(ip_source_string, ip_multicast_string)){
-            ogs_error("Could not create multicast route: %s to %s", ip_source_string, ip_multicast_string);
-        }else{
-            ogs_info("Multicast route created: %s to %s", ip_source_string, ip_multicast_string);
-            ogs_info("Join multicast group: %s", ip_multicast_string);
+            if(OGS_ERROR == multicastrouter_add_route_and_join(ip_source_string, ip_multicast_string)){
+                ogs_error("Could not create multicast route: %s to %s", ip_source_string, ip_multicast_string);
+            }else{
+                ogs_info("Multicast route created: %s to %s", ip_source_string, ip_multicast_string);
+                ogs_info("Join multicast group: %s", ip_multicast_string);
+            }
         }
     }
 
